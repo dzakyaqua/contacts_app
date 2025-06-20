f388c5a7a283db139677a0b5a205f10275254a5f
git config --global user.name dzakyaqua <dzaky3456sulthan@gmail.com>
2025-06-20 10:54:46 +0700

    Memanfaatkan React Context untuk Fitur Ubah Bahasa

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -6,6 +6,7 @@ import AddPage from '../pages/AddPage';
 import RegisterPage from '../pages/RegisterPage';
 import LoginPage from '../pages/LoginPage';
 import { getUserLogged, putAccessToken } from '../utils/api';
+import { LocaleProvider } from '../contexts/LocaleContext';
 
 class ContactApp extends React.Component {
   constructor(props) {
@@ -14,6 +15,19 @@ class ContactApp extends React.Component {
     this.state = {
       authedUser: null,
       initializing: true,
+      localeContext: {
+        locale: 'id',
+        toggleLocale: () => {
+          this.setState((prevState) => {
+            return {
+              localeContext: {
+                ...prevState.localeContext,
+                locale: prevState.localeContext.locale === 'id' ? 'en' : 'id'
+              }
+            }
+          })
+        }
+      }
     };
 
     this.onLoginSuccess = this.onLoginSuccess.bind(this);
@@ -59,6 +73,7 @@ class ContactApp extends React.Component {
 
     if (this.state.authedUser === null) {
       return (
+        <LocaleProvider value={this.state.localeContext}>
         <div className='contact-app'>
           <header className='contact-app__header'>
             <h1>Aplikasi Kontak</h1>
@@ -70,22 +85,27 @@ class ContactApp extends React.Component {
             </Routes>
           </main>
         </div>
+        </LocaleProvider>
+        
       )
     }
     
     return (
-      <div className="contact-app">
-        <header className='contact-app__header'>
-          <h1>Aplikasi Kontak</h1>
-          <Navigation logout={this.onLogout} name={this.state.authedUser.name} />
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/add" element={<AddPage />} />
-          </Routes>
-        </main>
-      </div>
+      <LocaleProvider value={this.state.localeContext}>
+        <div className="contact-app">
+          <header className='contact-app__header'>
+            <h1>Aplikasi Kontak</h1>
+            <Navigation logout={this.onLogout} name={this.state.authedUser.name} />
+          </header>
+          <main>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/add" element={<AddPage />} />
+            </Routes>
+          </main>
+        </div>
+      </LocaleProvider>
+      
     );
   }
 }
